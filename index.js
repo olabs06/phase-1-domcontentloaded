9aad9142a0e53622fd8bbe881fac2312e488649e
Simeon <olabs06@yahoo.com>
2022-06-25 11:41:17 +0100

    modifying index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let p = document.querySelector('p');
+    const event = document.createEvent( 'Event' );
+    p.textContent = "This is really cool!"
+  });
